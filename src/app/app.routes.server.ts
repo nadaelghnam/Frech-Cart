3678a26f1e659815692da0df582a5bd25655b960
nadaelghnam <nadaelghnam141@gmail.com>
2026-04-30 01:55:14 +0300

    solve error

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,19 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'product-details/:id',
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: 'payment/:id',
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: 'details/:id',
+    renderMode: RenderMode.Server,
+  },
+  {
     path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    renderMode: RenderMode.Prerender,
+  },
 ];
